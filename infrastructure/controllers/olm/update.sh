ae1eda7c3e30e44247309c249698dc96f5ac8257
Louis Orleans <louis@orleans.io>
2024-09-02 11:53:43 -0700

    ⬇️ downgrade to 0.26.0
    This should temporarily fix the `Recreate` / `rollingUpdate` issue

--- infrastructure/controllers/olm/update.sh
+++ infrastructure/controllers/olm/update.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
+version="latest/download"
+if [ -n "$1" ]; then
+  echo "Updating to version $1"
+  version="download/$1"
+fi
+
 curl \
   --silent \
-  -L https://github.com/operator-framework/operator-lifecycle-manager/releases/latest/download/olm.yaml \
+  -L https://github.com/operator-framework/operator-lifecycle-manager/releases/$version/olm.yaml \
 > "$(dirname $0)/olm.yaml"
 curl \
   --silent \
-  -L https://github.com/operator-framework/operator-lifecycle-manager/releases/latest/download/crds.yaml \
+  -L https://github.com/operator-framework/operator-lifecycle-manager/releases/$version/crds.yaml \
 > "$(dirname $0)/crds.yaml"
 
 echo "Updated OLM"
