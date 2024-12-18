6794b5d32b9bc0f2ab48670f704e82b3909c5e7a
Louis Orleans <louis@orleans.io>
2024-12-17 16:49:30 -0800

    ⬆️ switch from legacy OLM to v1

--- olm/update.sh
+++ olm/update.sh
@@ -1,18 +1,43 @@
 #!/usr/bin/env bash
+set -e
+
+# this might break in the future (which is probably good)
+# https://github.com/operator-framework/operator-controller/issues/1341
 
 version="latest/download"
 if [ -n "$1" ]; then
   echo "Updating to version $1"
   version="download/$1"
+else
+  echo "Updating to latest"
 fi
+
+# There are some duplicate resources defined in the yaml files downloaded below
+# In order to make them valid for fluxcd, we need to remove the duplicates
+duplicate_filter='
+  (.apiVersion == "v1" and .kind == "Namespace" and .metadata.name == "olmv1-system")
+  or (.apiVersion == "cert-manager.io/v1" and .kind == "ClusterIssuer" and .metadata.name == "olmv1-ca")
+  or (.apiVersion == "cert-manager.io/v1" and .kind == "Issuer" and .metadata.name == "self-sign-issuer")
+  or (.apiVersion == "cert-manager.io/v1" and .kind == "Certificate" and .metadata.name == "olmv1-ca")
+'
 
 curl \
   --silent \
-  -L https://github.com/operator-framework/operator-lifecycle-manager/releases/$version/olm.yaml \
-> "$(dirname $0)/olm.yaml"
+  --fail \
+  -L https://github.com/operator-framework/catalogd/releases/$version/catalogd.yaml \
+| yq eval ". | select(($duplicate_filter) | not)" \
+> "$(dirname $0)/catalogd.yaml"
 curl \
   --silent \
-  -L https://github.com/operator-framework/operator-lifecycle-manager/releases/$version/crds.yaml \
-> "$(dirname $0)/crds.yaml"
+  --fail \
+  -L https://github.com/operator-framework/catalogd/releases/$version/default-catalogs.yaml \
+> "$(dirname $0)/catalogs.yaml"
+
+curl \
+  --silent \
+  --fail \
+  -L https://github.com/operator-framework/operator-controller/releases/$version/operator-controller.yaml \
+| yq eval ". | select(($duplicate_filter) | not)" \
+> "$(dirname $0)/operator-controller.yaml"
 
 echo "Updated OLM"
