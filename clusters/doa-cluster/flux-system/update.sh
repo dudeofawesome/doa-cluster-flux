d7c7a94c5f6d764eccc5b047797be4693aef8468
Louis Orleans <louis@orleans.io>
2026-07-10 02:24:25 -0700

    👽️ update 1password ref

--- clusters/doa-cluster/flux-system/update.sh
+++ clusters/doa-cluster/flux-system/update.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-GITHUB_TOKEN="$(op item get 'Github PAT - dudeofawesome' --fields label=token)" \
+GITHUB_TOKEN="$(op read 'op://Private/4w4ctwnyxvw4wdtpymrgf4xvoy/credential')" \
   flux bootstrap github \
     --owner=dudeofawesome \
     --repository=doa-cluster-flux \
     --branch=main \
     --path=clusters/doa-cluster \
-    --personal
+    --personal \
+    $@
